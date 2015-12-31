4ced6d2a9b2deb4c3fbf5bd85dda5a819a5ad728
timothyfrancis <timothyfrancis.marano@gmail.com>
2015-12-31 03:37:07 +0000

    Create Big Code Mod.js

---
+++ Big Code Mod.js
@@ -0,0 +1,14 @@
+var welcomemsg = "Welcome";
+var ! = "!";
+var AgarModsversion = 8.99;
+var a;
+var message = Welcome+" "+!
+if (name == Tinoy) {
+var name = TKG;
+10000
+}
+var array = Mod;
+dothing function {
+if (name == un named cell) {
+var name = KILLER;
+}
