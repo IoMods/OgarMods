2df810ce5d3bdc92ab5327a1d52565434041c078
timothyfrancis <timothyfrancis.marano@gmail.com>
2016-01-04 07:28:57 +0000

    Update gitbook.js

--- gitbook.js
+++ gitbook.js
@@ -3,7 +3,7 @@ var booknore = repo,addon,100 P,{name: OgarBook}
 
 repo:
 
-http://github.com/timothyfrancis/Lengend
+http://github.com/timothyfrancis/LengendCoding-s-OBE
 
 http://timothyfrancis.gitbooks.io/OgarBook
 */
